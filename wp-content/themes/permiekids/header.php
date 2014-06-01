8734efda5ae10bbf7496c361bece9f9f9d19d504
itaddy <i.am.taddy@gmail.com>
2014-06-02 07:30:55 +0800

    Day 5 v3

--- wp-content/themes/permiekids/header.php
+++ wp-content/themes/permiekids/header.php
@@ -87,93 +87,103 @@
 									//Process nav menu using our custom nav walker
 									'walker' => new wp_bootstrap_navwalker())
 								);
-								?>						 	
+								?>	
+								<li><button class="search-icon">Search</button></li>
+						  </button>					 	
 						  </ul>
+
+						  <script>
+								jQuery( "button" ).click(function() {
+								  jQuery( ".search-toggle" ).slideToggle("slow");
+								});
+						  </script>
 					</div><!--/.nav-collapse -->
 
 			  </div>
 		</div>
 	</div>
 </div>
-<div class="line-separator"></div>
-<div class="container">
-<?php if (!is_front_page()) { ?>
-	<div class="the-search-container">
-		<div class="the-search-box">
-			<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-			<fieldset class="pk-search-tax-container pk-tax-ethics">
-				<h4>Ethics</h4>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="155" id="_ethic_155" name="_ethic[]">
-					<label for="_ethic_155" class="label label-default">Care of Earth</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="157" id="_ethic_157" name="_ethic[]">
-					<label for="_ethic_157" class="label label-default">Care of People</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="158" id="_ethic_158" name="_ethic[]">
-					<label for="_ethic_158" class="label label-default">Return of Surplus</label>
-	        	</div>
-			</fieldset>
-			<fieldset class="pk-search-tax-container pk-tax-principles">
-				<h4>Principles</h4>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="159" id="_principle_159" name="_principle[]">
-					<label for="_principle_159" class="label label-default">Apply Self-Regulation and Accept Feedback</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="160" id="_principle_160" name="_principle[]">
-					<label for="_principle_160" class="label label-default">Catch and Store Energy</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="161" id="_principle_161" name="_principle[]">
-					<label for="_principle_161" class="label label-default">Design from Patterns and Details</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="162" id="_principle_162" name="_principle[]">
-					<label for="_principle_162" class="label label-default">Integrate Rather than Segregate</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="163" id="_principle_163" name="_principle[]">
-					<label for="_principle_163" class="label label-default">Observe and Interact</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="156" id="_principle_156" name="_principle[]">
-					<label for="_principle_156" class="label label-default">Obtain a Yield</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="164" id="_principle_164" name="_principle[]">
-					<label for="_principle_164" class="label label-default">Produce No Waste</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="165" id="_principle_165" name="_principle[]">
-					<label for="_principle_165" class="label label-default">Small and Slow Solutions</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="166" id="_principle_166" name="_principle[]">
-					<label for="_principle_166" class="label label-default">Use and Value Resources</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="169" id="_principle_169" name="_principle[]">
-					<label for="_principle_169" class="label label-default">Use Creativity and Adapt to Change</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="167" id="_principle_167" name="_principle[]">
-					<label for="_principle_167" class="label label-default">Use the Edges and Value the Marginal</label>
-	        	</div>
-				<div class="pk-search-tax">
-					<input type="checkbox" value="168" id="_principle_168" name="_principle[]">
-					<label for="_principle_168" class="label label-default">Value Diversity</label>
-	        	</div>
+<div class="search-toggle">
+	<div class="line-separator"></div>
+	<div class="container">
+	<?php if (!is_front_page()) { ?>
+		<div class="the-search-container">
+			<div class="the-search-box">
+				<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+				<fieldset class="pk-search-tax-container pk-tax-ethics">
+					<h4>Ethics</h4>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="155" id="_ethic_155" name="_ethic[]">
+						<label for="_ethic_155" class="label label-default">Care of Earth</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="157" id="_ethic_157" name="_ethic[]">
+						<label for="_ethic_157" class="label label-default">Care of People</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="158" id="_ethic_158" name="_ethic[]">
+						<label for="_ethic_158" class="label label-default">Return of Surplus</label>
+					</div>
 				</fieldset>
-						 
-				<fieldset> 
-					 <input type="search" id="s" name="s" placeholder="Search" required />
-					 <input type="submit" id="searchsubmit" value="Search"  class="search_button"/>
-				 </fieldset>
-			</form>		
+				<fieldset class="pk-search-tax-container pk-tax-principles">
+					<h4>Principles</h4>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="159" id="_principle_159" name="_principle[]">
+						<label for="_principle_159" class="label label-default">Apply Self-Regulation and Accept Feedback</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="160" id="_principle_160" name="_principle[]">
+						<label for="_principle_160" class="label label-default">Catch and Store Energy</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="161" id="_principle_161" name="_principle[]">
+						<label for="_principle_161" class="label label-default">Design from Patterns and Details</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="162" id="_principle_162" name="_principle[]">
+						<label for="_principle_162" class="label label-default">Integrate Rather than Segregate</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="163" id="_principle_163" name="_principle[]">
+						<label for="_principle_163" class="label label-default">Observe and Interact</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="156" id="_principle_156" name="_principle[]">
+						<label for="_principle_156" class="label label-default">Obtain a Yield</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="164" id="_principle_164" name="_principle[]">
+						<label for="_principle_164" class="label label-default">Produce No Waste</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="165" id="_principle_165" name="_principle[]">
+						<label for="_principle_165" class="label label-default">Small and Slow Solutions</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="166" id="_principle_166" name="_principle[]">
+						<label for="_principle_166" class="label label-default">Use and Value Resources</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="169" id="_principle_169" name="_principle[]">
+						<label for="_principle_169" class="label label-default">Use Creativity and Adapt to Change</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="167" id="_principle_167" name="_principle[]">
+						<label for="_principle_167" class="label label-default">Use the Edges and Value the Marginal</label>
+					</div>
+					<div class="pk-search-tax">
+						<input type="checkbox" value="168" id="_principle_168" name="_principle[]">
+						<label for="_principle_168" class="label label-default">Value Diversity</label>
+					</div>
+					</fieldset>
+							 
+					<fieldset> 
+						 <input type="search" id="s" name="s" placeholder="Search" required />
+						 <input type="submit" id="searchsubmit" value="Search"  class="search_button"/>
+					 </fieldset>
+				</form>		
+			</div>
 		</div>
+	<?php } ?>
 	</div>
-<?php } ?>
-</div>
+</div>	
