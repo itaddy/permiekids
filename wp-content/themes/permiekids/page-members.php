2101add662f762acfa83bb6c0537309f2441e08c
itaddy <i.am.taddy@gmail.com>
2014-06-21 08:16:40 +0800

    Update on Functions.php
    fixed error in functions.php

---
+++ wp-content/themes/permiekids/page-members.php
@@ -0,0 +1,59 @@
+<?php 
+/*
+Template Name: Members List Template
+*/
+
+
+get_header(); ?>
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="line-separator"></div>	
+	<div class="container">
+		<div class="row">
+			<div class="title"><h1><?php the_title(); ?></h1></div>
+			<div class="breadcrumb"><?php the_breadcrumb(); ?></div>
+		</div>
+	</div>
+<div class="line-separator"></div>
+
+<div class="container">
+	<div class="row">
+		<div class="span8">
+			<?php 
+				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+					the_post_thumbnail();
+				} 
+			?>		
+			<?php
+				$blogusers = get_users( 'blog_id=1&orderby=nicename' );
+				// Array of WP_User objects.
+				foreach ( $blogusers as $user ) {
+					echo '<span>' . esc_html( $user->user_email ) . '</span>';
+				}
+			?>
+			<?php if ( is_active_sidebar( 'content-bottom-a' ) ) : ?>
+				<div class="span6">
+					<?php dynamic_sidebar( 'content-bottom-a' ); ?>
+				</div>		
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'content-bottom-b' ) ) : ?>
+				<div class="span6">
+				<?php dynamic_sidebar( 'content-bottom-b' ); ?>
+				</div>		
+			<?php endif; ?>			
+		 </div>
+		<?php endwhile; else: ?>
+		<div class="row">
+			<div class="span8">
+				<p><?php _e('Sorry, this page does not exist.'); ?></p>
+			</div>
+		</div>	
+		<?php endif; ?>
+		
+		<div class="span4">
+			<?php get_sidebar(); ?>	
+		</div>
+	</div>
+	
+<?php get_footer(); ?>
